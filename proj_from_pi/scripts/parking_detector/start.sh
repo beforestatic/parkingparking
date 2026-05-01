bca38b519b9b495c2abb227eab7055de38a48ab2
beforestatic <beforestatic@gmail.com>
2026-05-02 01:29:40 +0500

    Delete proj_from_pi directory

--- proj_from_pi/scripts/parking_detector/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export NAVIGATOR_API_URL=http://localhost:9000/ingest/parking
-export NAVIGATOR_API_KEY=demo-key-01
-export CAMERA_INDEX=0
-source .venv/bin/activate
-python3 gui_server.py --mode mock
